265e298c511d3e16c4c6aba3d7cf6842b1ecd7a5
sisini2anni <franck46grassini@gmail.com>
2025-11-17 14:46:55 +0000

    Matrici4

--- Esercizio.java
+++ Esercizio.java
@@ -14,28 +14,68 @@ class Esercizio {
 
     // Metodo per calcolare il massimo nella matrice
     public static Max calcolaMassimo( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+         public static record Max(int massimo, int rIdx, int cIdx) { }
+
+  
+    public static Max calcolaMassimo(int[][] Matrics, int rigons, int colons) 
+        int rIdx = 0;
+        int cIdx = 0;
+        for (int i = 0; i <= rigons - 1; i = i + 1) {
+            for (int j = 0; j <= colons - 1; j = j + 1) {
+                
+                if (M[i][j] > M[rIdx][cIdx]) {
+                    rIdx = i;
+                    cIdx = j;
+                }
+            }
+        }
+        int massimo = M[rIdx][cIdx];
+        return new Max(massimo, rIdx, cIdx);
+    }
     }
 
     // Metodo per calcolare la media di una colonna
-    public static float calcolaMedia( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO        
-    }
+    public static float calcolaMedia(int[][] Matrics, int rigons, int colons, int cIdx) 
+        float s = 0;
+        for (int i = 0; i <= rigons - 1; i = i + 1) {
+            s = s + M[i][cIdx];
+        }
+        float med = s / colons;
+        return med;
+    }       
+    
 
     // Metodo per riempire la matrice con valori casuali
     // Già risolto nell'esercizio precedente
-    public static void riempiCasuale( int[][] M, int RIGHE, int COLONNE, int valMin, int valMax) {
+    public static void riempiCasuale( int[][] Matrics, int rigons, int colons, int vmin, int valMax) {
         Random rand = new Random();
-        for (int i=0; i <= RIGHE-1; i=i+1 ) {
-            for (int j=0; j <= COLONNE-1; j=j+1) {
-                M[i][j] = valMin + rand.nextInt((valMax+1)-valMin);
+        for (int i=0; i <= rigons-1; i=i+1 ) {
+            for (int j=0; j <= colons-1; j=j+1) {
+                M[i][j] = vMin + rand.nextInt((valMax+1)-vMin);
             }
         }
     }
 
     public static void main(String args[]) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+         int G = 7;
+        int h = 5;
+        int[][] MG= new int[GIORNI][ORE];
+        
+        riempiCasuale(MG, G, h, 290, 300);
+        System.out.println("Temperature registrate:");
+        UtilsMatrice.visualizza(MG);
+         Max risultato = calcolaMassimo(MG, G, h);
+        System.out.println("La temperatura massima registrata è: " + risultato.massimo);
+        System.out.println("Giorno in cui è stata registrata: " + (risultato.rIdx() + 1) + "°");
+        System.out.println("Orario in cui è stata registrata" " + (risultato.cIdx() + 1) + "°");
+         System.out.println("La media delle Temperature è :");
+        for (int j = 0; j <= ORE - 1; j = j + 1) {
+            float mediaFasciaOraria = calcolaMedia(MG, G, h, j);
+            System.out.print(mediaFasciaOraria);
+        }
     }
+    
+    
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
